a0bef839ad7ed857ffc5a1031a048769000e8bcb
André Luiz <developer.andreluiz@gmail.com>
2021-03-17 18:18:41 -0300

    Projeto Carregado

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Trabalho_Deposito
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.txtValidade = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtFala = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblPorcentagem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quantidade";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantidade.Location = new System.Drawing.Point(17, 63);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(458, 32);
+            this.txtQuantidade.TabIndex = 0;
+            // 
+            // txtValidade
+            // 
+            this.txtValidade.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValidade.Location = new System.Drawing.Point(17, 124);
+            this.txtValidade.Name = "txtValidade";
+            this.txtValidade.Size = new System.Drawing.Size(458, 32);
+            this.txtValidade.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data de Validade";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(307, 162);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(168, 77);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // txtFala
+            // 
+            this.txtFala.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFala.Location = new System.Drawing.Point(17, 293);
+            this.txtFala.Multiline = true;
+            this.txtFala.Name = "txtFala";
+            this.txtFala.Size = new System.Drawing.Size(458, 148);
+            this.txtFala.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 267);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Texto";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(3, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(46, 19);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblPorcentagem
+            // 
+            this.lblPorcentagem.AutoSize = true;
+            this.lblPorcentagem.Location = new System.Drawing.Point(320, 9);
+            this.lblPorcentagem.Name = "lblPorcentagem";
+            this.lblPorcentagem.Size = new System.Drawing.Size(17, 19);
+            this.lblPorcentagem.TabIndex = 8;
+            this.lblPorcentagem.Text = "0";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 453);
+            this.Controls.Add(this.lblPorcentagem);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFala);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtValidade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Deposito";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtValidade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtFala;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblPorcentagem;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+
+namespace Trabalho_Deposito
+{
+    public partial class FrmMain : Form
+    {
+        private static SpeechRecognitionEngine engine;
+        static string falaValidade = string.Empty;
+        static string falaQuantidade = string.Empty;
+        static string falaQuantidadeP = string.Empty;
+
+
+
+        static bool validadeBool = false;
+        static bool quantidadeBool = true;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        public static void recValidade(object s,SpeechRecognizedEventArgs e)
+        {
+            if (validadeBool)
+            {
+                falaQuantidadeP = e.Result.Confidence.ToString();
+                falaValidade = e.Result.Text;
+            }
+        }
+
+        public static void recQuantidade(object s, SpeechRecognizedEventArgs e)
+        {
+            if (quantidadeBool)
+            {
+                falaQuantidade = e.Result.Text;
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            carregarVocabularioValidade();
+            carregarVocabularioQuantidade();
+        }
+
+
+        void carregarVocabularioValidade()
+        {
+            engine = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-BR"));
+            engine.SetInputToDefaultAudioDevice();
+            Choices dia = new Choices("primeiro", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dizesseis", "dizessete", "dizoito", "dizenove", "vinti", "vinti um", "vinti dois", "vinti três", "vinti quatro", "vinti cinco", "vinti seis", "vinti sete", "vinti oito", "vinti nove", "trinta", "trinte um");
+            Choices mes = new Choices("do um", "do dois", "do tres", "do quatro", "do cinco", "do seis", "do sete", "do oito", "do nove", "do dez","do onze","do doze");
+            Choices ano = new Choices("de dois mil dizenove", "de dois mil e vinte", "de dois mil e vinte um", "de dois mil e vinte dois", "de dois mil e vinte três", "de dois mil e vinte quatro", "de dois mil e vinte cinco");
+           
+            GrammarBuilder grammarBuilderValidade = new GrammarBuilder();
+            
+            grammarBuilderValidade.Append(dia);
+            grammarBuilderValidade.Append(mes);
+            grammarBuilderValidade.Append(ano);
+
+            Grammar grammar = new Grammar(grammarBuilderValidade);
+            engine.LoadGrammar(grammar);
+            
+            engine.RecognizeAsync(RecognizeMode.Multiple);
+            
+            engine.SpeechRecognized += recValidade;
+            
+            lblStatus.Text = "Ouvindo Validade";
+        }
+        void carregarVocabularioQuantidade()
+        {
+            engine = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-BR"));
+            engine.SetInputToDefaultAudioDevice();
+
+            Choices numero = new Choices("um","dois","três","quatro","cinco","seis","sete","oito","nove","zero");
+            
+
+            GrammarBuilder grammarBuilderNumero = new GrammarBuilder();
+
+            grammarBuilderNumero.Append(numero);
+           
+
+            Grammar grammar = new Grammar(grammarBuilderNumero);
+            engine.LoadGrammar(grammar);
+
+            engine.RecognizeAsync(RecognizeMode.Multiple);
+
+            engine.SpeechRecognized += recQuantidade;
+
+            lblStatus.Text = "Ouvindo Quantidade";
+        }
+     
+        
+        
+        
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(falaValidade)==false)
+            {
+                ExibirValidade();
+            }
+            if (string.IsNullOrEmpty(falaQuantidade)==false)
+            {
+                ExibirQuantidade();
+            }
+
+
+
+
+        }
+        void ExibirValidade()
+        {
+            string[] partes = falaValidade.Split(' ');
+            string etapaAno=string.Empty;
+
+
+
+            //falaValidade = string.Format("{0} {1} {2} {3} {4}", partes[0], partes[1], partes[2], partes[3], partes[4]);
+            //txtValidade.Text = falaValidade.Replace(" ", "");
+            lblPorcentagem.Text = falaQuantidadeP;
+            txtValidade.Text = falaValidade;
+            etapaAno = string.Empty;
+            falaValidade = string.Empty;
+        }
+        void ExibirQuantidade()
+        {
+            txtQuantidade.Text = falaQuantidade;
+            falaQuantidade = string.Empty;
+
+        }
+    }
+}
